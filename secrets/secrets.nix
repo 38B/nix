42389a36ac94c43b924b5e600f7e95953742edcc
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2022-10-25 13:14:58 -0400

    secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,10 @@
 let
   # set ssh public keys here for your system and user
-  system = "";
-  user = "";
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKdWofniKpekpXXmfHjxYs9n7t7vHjKk9B/vpOk2wVa";
+  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqbi+i1JttUl5UZd4zjtEyPRkvFqCsO2Sn9zR3HWOXs";
   allKeys = [ system user ];
 in
 {
   "secret.age".publicKeys = allKeys;
+  "blob-password.age".publicKeys = allKeys;
 }
