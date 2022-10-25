5be0c36db1758c1761d305de52e8272663527c0d
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2022-10-25 13:18:35 -0400

    add derviation for user blob

---
+++ users/blob/default.nix
@@ -0,0 +1,21 @@
+{ self, hmUsers, ... }:
+{
+  home-manager.users = { inherit (hmUsers) blob; };
+  age.secrets.blob-password.file = "${self}/secrets/blob-password.age";
+
+  users.users.blob =
+    let
+      passwordFile = "/run/agenix/blob-password";
+    in
+    {
+      passwordFile = passwordFile;
+      description = "default user with sudo powers"
+        isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+
+  home-manager.users.blob = { suites, ... }:
+    {
+      imports = suites.cli;
+    };
+}
